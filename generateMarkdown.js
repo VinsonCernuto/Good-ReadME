cc9332e443d8343eac343b6f08ce4dfe07ad68c8
Vinson Cernuto <cernutovinson@gmail.com>
2020-10-01 18:16:53 -0400

    taking a break for the night and pushing up changes

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,51 +1,27 @@
 // function to generate markdown for README
 function generateMarkdown(answers) {
   //Generate Table of contents based on user responses
+  let tableOfContents = `## Table of Contents`;
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  if (answers.installation !== '') {tableOfContents += `
+  *[installation](#installation)`};
+
+  if (answers.installation !== '') {tableOfContents += `
+  *[Usage](#Usage)`};
+
+  if (answers.installation !== '') {tableOfContents += `
+  *[contributing](#contributing)`};
+
+  if (answers.installation !== '') {tableOfContents += `
+  *[Tests](#Tests)`};
   
   return `
   # ${answers.title}
 
-  ## Description
+  ### Description
   ${answers.description}
 
-  ### Instalation
+  #### Instalation
 
 `;
 }
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+//external
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require('util');
+//internal
+const api = require("./api");
 const generateMarkdown = require("./generateMarkdown");
+const { userInfo } = require("os");
 
 
 inquirer.prompt([
+    {
+        type:"input",
+        message:"What is your GitHub username?",
+        name:"username"
+    },
     {
         type:"input",
         message:"Enter Project Title",
@@ -28,7 +38,7 @@ inquirer.prompt([
 
     },
     {
-        type:"input",
+        type:"list",
         message:"Choose a license for your project.",
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
         name:"license"
@@ -46,10 +56,43 @@ inquirer.prompt([
         name:"tests"
 
     },
-])
-.then(function(answers){
-    console.log(answers);
+    {
+        type:"input",
+        message:"Enter contact email",
+        name: "Email"
+    }
+]);
 
-const generateFile = generateMarkdown(answers)
+function writeToFile(fileName, answers){
+    fs.writeFile(fileName, answers, err =>{
+        if (err){
+            return console.log(err);
+        }
 
-});
+        console.log("README File has been created")
+    });
+}
+
+const writeAsync = untill.promisify(writeToFile);
+
+//function to collect everything needed
+async function init(){
+    try{
+        //user answers
+        const responses = await inquirer.prompt;
+        console.log("your responses: ", answers)
+
+        //call Github to find input username
+        const gitUserName = await api.getUser(userInfo);
+
+        //adding answers and github to generate markdown
+        const markdown = generateMarkdown(answers, userInfo);
+        console.log(markdown);
+
+        //write to file
+        await writeAsync("README.md, markdown")
+    
+    } catch(error){
+        console.log(error);
+    }
+};
