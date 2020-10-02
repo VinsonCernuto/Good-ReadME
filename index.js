5599384859c4e35fb74cb74632c582c9065a5094
Vinson Cernuto <cernutovinson@gmail.com>
2020-10-02 16:27:21 -0400

    changed how github is called and how file is written learned an easyier way in class today

--- index.js
+++ index.js
@@ -2,97 +2,73 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require('util');
-//internal
-const api = require("./api");
 const generateMarkdown = require("./generateMarkdown");
-const { userInfo } = require("os");
+const writeFileAsync = until.promisify(fs.writeFile)
 
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter Project Title",
+            name:"title" 
+        },
+        {
+            type:"input",
+            message:"Write a description of your project",
+            name:"description"
 
-inquirer.prompt([
-    {
-        type:"input",
-        message:"What is your GitHub username?",
-        name:"username"
-    },
-    {
-        type:"input",
-        message:"Enter Project Title",
-        name:"title" 
-    },
-    {
-        type:"input",
-        message:"Write a description of your project",
-        name:"description"
+        },
+        {
+            type:"input",
+            message:"If installation steps rquired, describe the steps required to install your project for the Installation section.",
+            name:"installation"
 
-    },
-    {
-        type:"input",
-        message:"If installation steps rquired, describe the steps required to install your project for the Installation section.",
-        name:"installation"
+        },
+        {
+            type:"input",
+            message:"Provide instructions and examples of your project in use for the Usage section.",
+            name:"usage"
 
-    },
-    {
-        type:"input",
-        message:"Provide instructions and examples of your project in use for the Usage section.",
-        name:"usage"
+        },
+        {
+            type:"list",
+            message:"Choose a license for your project.",
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+            name:"license"
 
-    },
-    {
-        type:"list",
-        message:"Choose a license for your project.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name:"license"
+        },
+        {
+            type:"input",
+            message:"If applicable, provide guidelines on how other developers can contribute to your project.",
+            name:"contributing"
 
-    },
-    {
-        type:"input",
-        message:"If applicable, provide guidelines on how other developers can contribute to your project.",
-        name:"contributing"
+        },
+        {
+            type:"input",
+            message:"If applicable, provide any tests written for your application and provide examples on how to run them.",
+            name:"tests"
 
-    },
-    {
-        type:"input",
-        message:"If applicable, provide any tests written for your application and provide examples on how to run them.",
-        name:"tests"
-
-    },
-    {
-        type:"input",
-        message:"Enter contact email",
-        name: "Email"
-    }
-]);
-
-function writeToFile(fileName, answers){
-    fs.writeFile(fileName, answers, err =>{
-        if (err){
-            return console.log(err);
+        },
+        {
+            type:"input",
+            message:"What is your GitHub username?",
+            name:"username"
+        },
+        {
+            type:"input",
+            message:"Enter contact email",
+            name: "Email"
         }
-
-        console.log("README File has been created")
-    });
+    ])
 }
+//function using until.promisify
+    async function init(){
+        //ask thw question and generate the answers
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        //writes the read me file to generated read directory
+        await writeFileAsync('./generatedReadme/README.md', generateContent);
+        console.log("Successfully wrote README.md");
+    }  
 
-const writeAsync = untill.promisify(writeToFile);
-
-//function to collect everything needed
-async function init(){
-    try{
-        //user answers
-        const responses = await inquirer.prompt;
-        console.log("your responses: ", answers)
-
-        //call Github to find input username
-        const gitUserName = await api.getUser(userInfo);
-
-        //adding answers and github to generate markdown
-        const markdown = generateMarkdown(answers, userInfo);
-        console.log(markdown);
-
-        //write to file
-        await writeAsync("README.md, markdown")
-    
-    } catch(error){
-        console.log(error);
-    }
-};
+init();
